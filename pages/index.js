9f934a111ba4d6813a0b222da4d787755b80faa4
johnpolacek <johnpolacek@gmail.com>
2020-09-11 17:23:53 -0500

    remove Layout component

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
-import Layout from "../components/Layout"
 import { MDXProvider } from "@mdx-js/react"
+import Head from "../components/Head"
 import Animation from "../components/Animation"
 import ReadMe from "../README.md"
 
 const IndexPage = () => (
-  <Layout
-    url="/"
-    title="Growth Areas for a Software Developer"
-    description="A list of areas to focus on in and examples to follow in order to grow your career as a software developer."
-  >
+  <>
+    <Head
+      url="/"
+      title="Growth Areas for a Software Developer"
+      description="A list of areas to focus on in and examples to follow in order to grow your career as a software developer."
+    />
     <Animation />
     <MDXProvider>
       <ReadMe />
     </MDXProvider>
-  </Layout>
+  </>
 )
 export default IndexPage
