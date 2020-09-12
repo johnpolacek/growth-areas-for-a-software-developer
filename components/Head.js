51101fedca4f82b95b37e51867f3364fe80e0c95
johnpolacek <johnpolacek@gmail.com>
2020-09-12 10:42:58 -0500

    formatting

--- components/Head.js
+++ components/Head.js
@@ -1,8 +1,8 @@
 import Head from "next/head"
 
-const url="/"
-const title="Growth Areas for a Software Developer"
-const description="A list of areas to focus on in and examples to follow in order to grow your career as a software developer."
+const url = "/"
+const title = "Growth Areas for a Software Developer"
+const description = "A list of areas to focus on in and examples to follow in order to grow your career as a software developer."
 const twitter = "@johnpolacek"
 const imageUrl = "https://growth-areas-for-a-software-developer.vercel.app/screenshot.png"
 const imageAlt = "5 Growth Areas for a Software Developer"
@@ -19,10 +19,7 @@ const DocHead = () => (
       {imageUrl && <meta property="og:image" content={imageUrl} />}
       {imageAlt && <meta property="og:image:alt" content={imageAlt} />}
       <meta property="og:url" content={url} />
-      <meta
-        name="twitter:card"
-        content={imageUrl ? "summary_large_image" : "summary"}
-      />
+      <meta name="twitter:card" content={imageUrl ? "summary_large_image" : "summary"} />
       <meta name="twitter:site" content={twitter} />
       <meta name="twitter:creator" content={twitter} />
       <meta name="twitter:title" content={title} />
